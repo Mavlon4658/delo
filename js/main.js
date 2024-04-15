2f6280a2f7cf3812c4c454fdd6e3727c982034bf
Mavlon4658 <72940597+Mavlon4658@users.noreply.github.com>
2024-04-16 00:44:57 +0500

    project ready

---
+++ js/main.js
@@ -0,0 +1,76 @@
+let header = document.querySelector('.header');
+
+if (scrollY > 200) {
+    header.classList.add('active');
+} else {
+    header.classList.remove('active');
+}
+
+document.addEventListener('scroll', function () {
+    if (scrollY > 200) {
+        header.classList.add('active');
+    } else {
+        header.classList.remove('active');
+    }
+}) 
+
+let homeSwiper = new Swiper('.home .swiper', {
+    direction: "horizontal",
+    slidesPerView: 1,
+    spaceBetween: 0,
+    parallax: true,
+    speed: 1000,
+    autoplay: {
+        delay: 3000,
+    },
+    allowTouchMove: false,
+    breakpoints: {
+        1200: {
+            direction: "vertical"
+        }
+    },
+    pagination: {
+        el: '.home .swiper_pagination',
+        clickable: true,
+    }
+})
+
+let fraction = document.querySelector('.home .swiper_fraction');
+for (let i = 0; i < homeSwiper.slides.length; i++) {
+    let btn = document.createElement('button');
+    btn.textContent = i < 10 ? `0${i}` : i;
+    if (i == 0) {
+        btn.classList.add('active');
+    }
+    fraction.appendChild(btn);
+    btn.onclick = () => {
+        homeSwiper.slideTo(i)
+    }
+}
+
+homeSwiper.on('slideChange', function (e) {
+    let i = homeSwiper.activeIndex;
+    document.querySelectorAll('.home .swiper_fraction button').forEach((el, idx) => {
+        if (i == idx) {
+            el.classList.add('active');
+        } else {
+            el.classList.remove('active');
+        }
+    })
+});
+
+let bars = document.querySelector('header .bars');
+let mobileMenu = document.querySelector('.mobile_menu');
+
+bars.onclick = () => {
+    mobileMenu.classList.toggle('active');
+    bars.classList.toggle('active');
+}
+
+let showMore = document.querySelector('.robot .show_more')
+let description = document.querySelector('.robot__left p');
+
+showMore.onclick = () => {
+    description.classList.add('active');
+    showMore.classList.add('hidden')
+}
